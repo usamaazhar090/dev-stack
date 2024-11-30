e6732c644e1dbfda0eb66cd8d9ecd00ce1c8ceff
usamaazhar090 <usamaazhar090@google.com>
2024-11-30 16:06:23 +0500

    in this branch we setup eslint and prettier for the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import React from "react";
+import "./globals.css";
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: 'Dev Stack',
+  title: "Dev Stack",
   description:
-    'A website where you can handle your worries by sharing with others',
+    "A website where you can handle your worries by sharing with others",
 };
 
 export default function RootLayout({
@@ -25,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
